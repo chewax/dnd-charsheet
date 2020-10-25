b9261efeeacf0b26554ed096f5e47aa4e1f107a6
Daniel <dwaksman@gmail.com>
2020-10-25 13:13:35 -0300

    Initial Commit

---
+++ src/components/TextInput.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    class="text-input"
+    v-bind:class="{
+      xs: xs
+    }"
+  >
+    <input v-model="inputValue" @input="onInputChange()" type="text" />
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    msg: String,
+    modelValue: String,
+    xs: Boolean
+  },
+  data: function() {
+    return {
+      inputValue: this.value
+    };
+  },
+  methods: {
+    onInputChange() {
+      this.$emit("update:modelValue", this.inputValue);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+$xsmall-screen: "only screen and (max-width : 350px)";
+$small-screen: "only screen and (max-width : 600px)";
+$large-screen: "only screen and (min-width : 950px)";
+$xlarge-screen: "only screen and (min-width : 1500px)";
+
+.text-input {
+  display: flex;
+  margin-bottom: 35px;
+  margin-left: 1vw;
+  margin-right: 1vw;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 40vw;
+
+  @media #{$small-screen} {
+    width: 90vw;
+  }
+
+  @media #{$large-screen} {
+    width: 27vw;
+  }
+
+  @media #{$xlarge-screen} {
+    width: 20vw;
+  }
+}
+
+.xs {
+  width: 70px;
+
+  @media #{$small-screen} {
+    width: 25vw;
+  }
+
+  @media #{$xsmall-screen} {
+    width: 38vw;
+  }
+}
+
+input {
+  border: none;
+  border-bottom: 1px solid #5a5a5a;
+  font-size: 12px;
+  font-weight: 400;
+  width: 100%;
+  // font-family: Verdana, Geneva, Tahoma, sans-serif;
+  text-transform: uppercase;
+}
+
+span {
+  text-transform: uppercase;
+  font-size: 8px;
+  font-weight: 700;
+  // font-family: Verdana, Geneva, Tahoma, sans-serif;
+  letter-spacing: 2px;
+  margin-top: 5px;
+  margin-left: 3px;
+}
+</style>
---
+++ src/views/CharSheet.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="csheet">
+    <div class="base-info">
+      <text-input
+        msg="Nombre del Personaje"
+        v-model="character.name"
+      ></text-input>
+
+      <text-input msg="Jugador" v-model="character.player"></text-input>
+
+      <text-input
+        msg="Clase y Nivel"
+        v-model="character.class_level"
+      ></text-input>
+
+      <text-input msg="Raza" v-model="character.race" xs></text-input>
+      <text-input
+        msg="Alineamiento"
+        v-model="character.alignment"
+        xs
+      ></text-input>
+
+      <text-input msg="Deidad" v-model="character.deity" xs></text-input>
+      <text-input msg="Tamano" v-model="character.size" xs></text-input>
+      <text-input msg="Edad" v-model="character.age" xs></text-input>
+      <text-input msg="Sexo" v-model="character.gender" xs></text-input>
+      <text-input msg="Altura" v-model="character.height" xs></text-input>
+      <text-input msg="Peso" v-model="character.weight" xs></text-input>
+      <text-input msg="Ojos" v-model="character.eyes" xs></text-input>
+      <text-input msg="Cabello" v-model="character.hair" xs></text-input>
+      <text-input msg="Piel" v-model="character.skin" xs></text-input>
+    </div>
+
+    {{ character }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.base-info {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+</style>
+  
+<script>
+import TextInput from "@/components/TextInput.vue";
+
+export default {
+  components: {
+    TextInput
+  },
+  data: () => ({
+    character: {}
+  })
+};
+</script>
